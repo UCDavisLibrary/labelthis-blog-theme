82a6b36b97598dbda8944bd1094e557341d2d428
Pablo Defendini <pablo@defendini.com>
2016-06-21 09:30:35 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+?>
+
+	</div>
+
+	<!-- Begin Footer -->
+<footer id="footer" class="source-org vcard copyright" role="contentinfo">
+	<nav>
+		<div class="secondary-menu">
+			<ul class="menu">
+				<li class="menu-item">
+					<a target="_blank" href="https://labelthis.lib.ucdavis.edu/users/sign_in">Admin login</a>
+				</li>
+				<li class="menu-item">
+					<a href="#FIXME">Questions or Comments?</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<section class="row align-center donor-links">
+		<div><img src="https://labelthis.lib.ucdavis.edu/images/uc-lib-logo-label-this.svg"></a></div>
+		<small>This site was built for the <a href="https://www.lib.ucdavis.edu/dept/specol/">Special Collections Department</a> at the <a href="https://www.lib.ucdavis.edu">UC Davis Library</a>.<br/>
+ 					Help us make this and other projects possible—<a href="https://give.ucdavis.edu/ulib/">support the Library.</a></small>
+	</section>
+	<section class="row align-center ucd-links">
+		<small class="legal"><a href="https://www.ucdavis.edu">University of California, Davis</a>, One Shields Avenue, Davis, CA 95616 | 530-752-1011<br/>
+			&copy;<?php echo date("Y"); ?> The Regents of the <a href="http://www.universityofcalifornia.edu/">University of California</a>, Davis campus. All rights reserved.</small>
+	</section>
+	<section class="row align-spaced legal-links">
+		<div><small class="legal"><a href="https://www.ucdavis.edu/privacy-and-accessibility">Privacy & Accessibility</a></small></div>
+		<div><small class="legal"><a href="http://occr.ucdavis.edu/poc/">Principles of Community</a></small></div>
+	</section>
+</footer>
+<nav id="small-menu">
+	<ul class="sm-menu">
+		<li class="sm-menu-item close-button">
+			<a href="#large-menu">&times;</a>
+		</li>
+		<li class="sm-menu-item">
+			<a href="https://labelthis.lib.ucdavis.edu">Home</a>
+		</li>
+		<li class="sm-menu-item">
+			<a href="https://labelthis.lib.ucdavis.edu/mark">Mark</a>
+		</li>
+		<li class="sm-menu-item">
+			<a href="https://labelthis.lib.ucdavis.edu/transcribe">Transcribe</a>
+		</li>
+		<li class="sm-menu-item">
+			<a href="#FIXME">Search</a>
+		</li>
+		<li class="sm-menu-item">
+			<a href="https://labelthis.lib.ucdavis.edu/#">About the Project</a>
+		</li>
+		<li class="sm-menu-item">
+			<a target="_blank" href="#FIXME">Blog</a>
+		</li>
+		<li class="sm-menu-item">
+			<a target="_blank" href="#FIXME">Questions or Comments?</a>
+		</li>
+		<li class="sm-menu-item">
+			<a target="_blank" href="https://labelthis.lib.ucdavis.edu/users/sign_in">Admin login</a>
+		</li>
+	</ul>
+</nav>
+<!-- End Footer -->
+
+	<?php wp_footer(); ?>
+
+<!-- jQuery is called via the WordPress-friendly way via functions.php -->
+
+<!-- this is where we put our custom functions -->
+<script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
+
+<!-- Asynchronous google analytics; this is the official snippet.
+         Replace UA-XXXXXX-XX with your site's ID and domainname.com with your domain, then uncomment to enable.
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-XXXXXX-XX', 'domainname.com');
+  ga('send', 'pageview');
+
+</script>
+-->
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+ get_header(); ?>
+      <section class="blog-header small-12 columns">
+    		<img src="https://prototypes.lib.ucdavis.edu/images/label-this-blog-title-image.svg" alt="Label This Project title image"/>
+    		<!-- img class="small-title" src="../../images/label-this-title-image-sm.svg" alt=""-->
+    	</section>
+      <section class="blog-river small-12 medium-7 medium-offset-1 large-4 large-offset-3 columns">
+      	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      		  <div class="blog-post-item blog-post-item-featured">
+      		    <h4><?php posted_on(); ?></h4>
+      		    <h1><a href="<?php the_permalink() ?>" <?php post_class() ?> id="post-<?php the_ID(); ?>"><?php the_title(); ?></a></h1>
+      		    <div class="row">
+      		      <!--div class="small-4">
+                  <img src="../../images/fpo_square.png" alt="Square Thumbnail">
+                </div -->
+      		      <div>
+      		        <?php the_excerpt(); ?>
+      		      </div>
+      		    </div>
+      		  </div>
+
+      	<?php endwhile; ?>
+        <?php post_navigation(); ?>
+        <?php else : ?>
+
+          <h2><?php _e('Nothing Found','html5reset'); ?></h2>
+
+        <?php endif; ?>
+      </section>
+      <section class="blog-sidebar small-12 medium-3 large-2 columns">
+        <?php get_sidebar(); ?>
+      </section>
+
+    </section> <!-- blog row -->
+<?php get_footer(); ?>
